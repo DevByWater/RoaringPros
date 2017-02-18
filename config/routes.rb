c3c91bbd92fab2be00fe619c507ded74ee7fa317
DevByWater <egaraudy@gmail.com>
2017-02-18 16:51:09 -0700

    Update User and Profile models Set up authenticated routes
    Signed-off-by: DevByWater <egaraudy@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :profiles
+
   devise_for :users
+
+  authenticated :user do
+    root 'profile#index'
+    resources :profiles, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+
+  resources :profiles, only: [:show]
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/migrate/20170217221545_create_profiles.rb
+++ db/migrate/20170217221545_create_profiles.rb
@@ -1,11 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :profiles do |t|
-      t.string :passion
-      t.string :position
-      t.string :offering
-      t.string :location
-      t.string :bio
+      t.integer :user_id
+
+      t.string :passion, null: true
+      t.string :position, null: true
+      t.string :offering, null: true
+      t.string :location, null: true
+      t.string :bio, null: true
 
 
       t.timestamps
